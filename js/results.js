d23c03a14965c70e117cc8355903fee803bb5304
NoreeGit <noreegit@gmail.com>
2025-05-26 18:15:09 +0100

    Update results.js

--- js/results.js
+++ js/results.js
@@ -1,5 +1,3 @@
-// results.js content/* js/results.js */
 const resultContainer = document.getElementById('results-summary');
 const finalScore = localStorage.getItem('quizScore');
-
-resultContainer.innerHTML = `<p>You scored ${finalScore} out of 3.</p>`;
+resultContainer.innerHTML = `<p>You scored ${finalScore} out of ${3}.</p>`;
